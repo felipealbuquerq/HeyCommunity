23e30945b1751ed7fba715c0376c7a736739aaa0
supeRodv2 <supgeek.rod@gmail.com>
2017-03-13 12:34:43 +0800

    some change to the release

--- .release.sh
+++ .release.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-rm -rf backend/vendor \
-    backend/public/uploads \
-    backend/public/bower-assets \
-    backend/storage
+cd backend
+find storage -name "[^.]*" -delete
+rm -rf vendor
+rm -rf .env .idea
 
-mkdir -p backend/storage/app/uploads/avatars \
-    backend/storage/app/uploads/timeline \
-    backend/storage/framework/cache \
-    backend/storage/framework/views \
-    backend/storage/framework/sessions \
-    backend/storage/logs
 
-rm -rf frontend/platforms \
-    frontend/plugins \
-    frontend/node_modules \
-    frontend/.tmp
+cd ../frontend
+rm -rf platforms plugins node_modules \
+    .tmp .sass-cache .idea npm-debug.log
+find hooks -name "[^R]*" -delete
 
+
+cd ../
 find . -name ".DS_Store" -delete
 find . -name ".sw[mnpcod]" -delete
+find . -name ".git.bak" | xargs rm -rf
 
 
 # rm -rf .git backend/.git frontend/.git .release.sh
