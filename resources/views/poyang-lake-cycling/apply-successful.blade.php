62922255a11912dbe77e55e8f2ad028d79ef2d56
Rod <supgeek.rod@gmail.com>
2018-09-06 14:18:41 +0800

    optimizations

--- resources/views/poyang-lake-cycling/apply-successful.blade.php
+++ resources/views/poyang-lake-cycling/apply-successful.blade.php
@@ -17,7 +17,7 @@
                     </h1>
 
                     <p class="mt-4 text-center">
-                        恭喜 <b>{{ $applyData->name }}</b>，您已成功报名 <em>{{ \App\PoYangLakeCyclingApplyData::$groups[$applyData->group_id] }}</em> 组
+                        恭喜<b>{{ $applyData->name }}</b>，您已成功报名 [<em>{{ \App\PoYangLakeCyclingApplyData::$groups[$applyData->group_id] }}</em>] 组
                         <br>
                         遵守赛事规则，祝您取得好成绩
                     </p>
