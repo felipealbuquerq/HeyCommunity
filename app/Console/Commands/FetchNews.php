3da212889603ee511e11a7ad615cf62c9c78b2cf
Rod <supgeek.rod@gmail.com>
2018-10-24 17:14:06 +0800

    Fetch News

--- app/Console/Commands/FetchNews.php
+++ app/Console/Commands/FetchNews.php
@@ -12,14 +12,17 @@ class FetchNews extends Command
      *
      * @var string
      */
-    protected $signature = 'fetch:news';
+    protected $signature = 'fetch:news
+                             {--startPage=1 : Start on what page}
+                             {--totalPage=1 : How many pages}
+     ';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'fetch news';
 
     /**
      * Create a new command instance.
@@ -38,38 +41,49 @@ class FetchNews extends Command
      */
     public function handle()
     {
-        $api = env('FETCH_NEWS_API');
-        $headers = ['Authorization' => 'APPCODE ' . env('FETCH_NEWS_APPCODE', 'null')];
+        $startPage = $this->option('startPage');
+        $startPage--;
+        $totalPage = $this->option('totalPage');
 
-        $client = new \GuzzleHttp\Client();
-        try {
-            $response = $client->request('GET', $api, [
-                'headers'   =>  $headers,
-            ]);
-        } catch (\GuzzleHttp\Exception\RequestException $exception) {
-            echo $exception;
-            report($exception);
-            return false;
-        }
+        foreach (range(1, $totalPage) as $num) {
+            $startPage++;
 
-        if ($response->getStatusCode() == 200) {
-            $content = $response->getBody()->getContents();
-            $data = json_decode($content)->showapi_res_body->pagebean->contentlist;
+            $api = env('FETCH_NEWS_API') . '&page=' . $startPage;
+            echo $api;
+            echo "\n";
 
-            foreach ($data as $index => $news) {
-                $newsData = [
-                    'origin'        =>  $news->source,
-                    'category'      =>  $news->channelName,
-                    'title'         =>  $news->title,
-                    'content'       =>  $news->html,
-                    'avatar'        =>  $news->havePic ? $news->imageurls[0]->url : '',
-                    'gallery'       =>  json_encode($news->imageurls),
-                    'url'           =>  $news->link,
-                    'weburl'        =>  $news->link,
-                    'time'          =>  $news->pubDate,
-                ];
+            $headers = ['Authorization' => 'APPCODE ' . env('FETCH_NEWS_APPCODE', 'null')];
+            $client = new \GuzzleHttp\Client();
 
-                News::updateOrCreate($newsData);
+            try {
+                $response = $client->request('GET', $api, [
+                    'headers'   =>  $headers,
+                ]);
+            } catch (\GuzzleHttp\Exception\RequestException $exception) {
+                echo $exception;
+                report($exception);
+                return false;
+            }
+
+            if ($response->getStatusCode() == 200) {
+                $content = $response->getBody()->getContents();
+                $data = json_decode($content)->showapi_res_body->pagebean->contentlist;
+
+                foreach ($data as $index => $news) {
+                    $newsData = [
+                        'origin'        =>  $news->source,
+                        'category'      =>  $news->channelName,
+                        'title'         =>  $news->title,
+                        'content'       =>  $news->html,
+                        'avatar'        =>  $news->havePic ? $news->imageurls[0]->url : '',
+                        'gallery'       =>  json_encode($news->imageurls),
+                        'url'           =>  $news->link,
+                        'weburl'        =>  $news->link,
+                        'time'          =>  $news->pubDate,
+                    ];
+
+                    News::updateOrCreate($newsData);
+                }
             }
         }
     }
