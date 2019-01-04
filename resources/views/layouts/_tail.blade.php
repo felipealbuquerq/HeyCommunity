e72eba144f4ab1c6d1783e4423d26472a5a56517
Rod <supgeek.rod@gmail.com>
2019-01-04 11:39:35 +0800

    site map

--- resources/views/layouts/_tail.blade.php
+++ resources/views/layouts/_tail.blade.php
@@ -1,5 +1,15 @@
 <div class="card card-link-list">
     <div class="card-body">
+        <!-- Site Map -->
+        <div class="d-none">
+            <a class="ml-0" href="{{ route('news.index') }}">新闻</a>
+            <a href="{{ route('daily.index') }}">今日</a>
+            <a href="{{ route('post.index') }}">资讯</a>
+            <a href="{{ route('columnist.index') }}">专栏</a>
+            <a href="{{ route('topic.index') }}">话题</a>
+            <a href="{{ route('activity.index') }}">活动</a>
+        </div>
+
         <div class="text-nowrap text-overflow">
             &copy; 2018 {{ $system->site_title }}
             <small>免费开源的社交软件</small>
