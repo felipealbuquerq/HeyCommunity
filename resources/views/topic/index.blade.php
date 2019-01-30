413e773e9b015bee5fc52f980e1d1555a3cf3312
Rod <supgeek.rod@gmail.com>
2019-01-30 16:57:33 +0800

    topic index reload btn

--- resources/views/topic/index.blade.php
+++ resources/views/topic/index.blade.php
@@ -62,7 +62,7 @@
                     <a class="btn btn-secondary {{ setParamActive('filter', 'noreply') }}" href="{{ route('topic.index', ['filter' => 'noreply']) }}">零回复</a>
 
                     <div class="pull-right d-none d-sm-block">
-                        <a class="btn btn-secondary" href="{{ route('topic.index', ['filter' => 'default']) }}">刷新</a>
+                        <a class="btn btn-secondary" href="#" onclick="document.location.reload()">刷新</a>
                     </div>
                 </div>
 
