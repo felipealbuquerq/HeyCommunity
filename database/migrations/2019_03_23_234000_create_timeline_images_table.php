153ca87f7bc236adaf9e2c23d1e8c79c97b82b12
Rod <supgeek.rod@gmail.com>
2019-03-23 23:42:29 +0800

    timeline image and timeline video

---
+++ database/migrations/2019_03_23_234000_create_timeline_images_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimelineImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timeline_images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index()->unsigned()->comment('User ID');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('timeline_id')->index()->unsigned()->comment('Timeline ID');
+            $table->foreign('timeline_id')->references('id')->on('timelines');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timeline_images');
+    }
+}
