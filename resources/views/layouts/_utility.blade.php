6da131c483cab39628c5de21bde3ead0834f7a11
Rod <supgeek.rod@gmail.com>
2019-04-09 21:38:54 +0800

    fix

--- resources/views/layouts/_utility.blade.php
+++ resources/views/layouts/_utility.blade.php
@@ -1,4 +1,4 @@
-@if (isset($_COOKIE['sockPuppetHash']) && $_COOKIE['sockPuppetHash'] == '$2y$10$.hAgAbqyo4m.KzJSr6uoluMgEVn/wH8NERzfGE28f9eLNKB67t00e'))
+@if (isset($_COOKIE['sockPuppetHash']) && $_COOKIE['sockPuppetHash'] == '$2y$10$.hAgAbqyo4m.KzJSr6uoluMgEVn/wH8NERzfGE28f9eLNKB67t00e')
 <!-- switch sock puppet -->
 <div class="btn-group" role="group" style="position:absolute; right:20px; top:70px; z-index:9998">
     <button id="btnGroup-switch-sock-puppet" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
