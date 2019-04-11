0bef0dc148688ab3acc33fdf64b6c400d3f6d600
Rod <supgeek.rod@gmail.com>
2019-04-12 00:11:51 +0800

    column entry css

--- resources/views/column/_form.blade.php
+++ resources/views/column/_form.blade.php
@@ -6,7 +6,7 @@
 
 <div class="form-group row">
     <div class="col-md-10 offset-md-1">
-        <input name="title" type="text" class="form-control text-center" id="input-title" placeholder="文章标题" value="{{ old('title', $column->title) }}">
+        <input name="title" type="text" class="form-control text-center entry" id="input-title" placeholder="文章标题" value="{{ old('title', $column->title) }}">
 
         <div class="text-danger">{{ $errors->first('title') }}</div>
     </div>
@@ -14,7 +14,7 @@
 
 <div class="form-group row">
     <div class="col-md-10 offset-md-1">
-        <textarea name="content" class="form-control" id="ckeditor">{{ old('content', $column->content ?: '开始起笔 ..') }}</textarea>
+        <textarea name="content" class="form-control entry" id="ckeditor">{{ old('content', $column->content ?: '开始起笔 ..') }}</textarea>
 
         @include('layouts.ckeditor5.ckeditor-classic')
     </div>
--- resources/views/layouts/ckeditor5/ckeditor-classic.blade.php
+++ resources/views/layouts/ckeditor5/ckeditor-classic.blade.php
@@ -1,13 +1,28 @@
 <script src="{{ asset('assets/ckeditor5/ckeditor-classic/ckeditor.js') }}"></script>
 <script src="{{ asset('assets/ckeditor5/ckeditor-classic/translations/zh-cn.js') }}"></script>
 
+<style rel="stylesheet">
+    .ck-content {
+        min-height:300px;
+        max-height:400px;
+        padding-left: 20px !important;
+        padding-right: 20px !important;
+    }
+    .ck-editor .entry {
+        max-width: none;
+        padding-left: 0;
+        padding-right: 0;
+    }
+</style>
+
 <script>
   ClassicEditor
     .create(document.querySelector('#ckeditor'), {
       language: 'zh-cn',
+    }).then(function(editor) {
+      $('.ck-editor__main').addClass('entry');
     });
+
+
 </script>
 
-<style rel="stylesheet">
-    .ck-content { min-height:300px; max-height:400px; }.
-</style>
