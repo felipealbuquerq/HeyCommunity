e298fb0d3356e3e413af58be9b94309ef7e632b3
Rod <supgeek.rod@gmail.com>
2019-04-18 11:03:51 +0800

    add vue commponents

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,6 +6,7 @@
  */
 
 // require('./bootstrap');
+// window.Vue = require('vue');
 require('./laravel-echo');
 
 require('./utilities/request');
@@ -19,3 +20,17 @@ $(document).ready(function() {
     },
   });
 });
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+// Vue.component('example', require('./components/Example.vue'));
+
+/*
+const app = new Vue({
+    el: '#app'
+});
+ */
