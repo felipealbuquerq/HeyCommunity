ea23095f647a96721d823c9ded826eb17d624a98
Rod <supgeek.rod@gmail.com>
2019-04-23 11:36:01 +0800

    user home

--- resources/views/user/uhome/_nav.blade.php
+++ resources/views/user/uhome/_nav.blade.php
@@ -1,7 +1,20 @@
-<nav class="nav nav-pills justify-content-end" style="position:relative;" id="mainTab">
+<nav class="d-none d-md-flex nav nav-pills justify-content-end" style="position:relative;" id="mainTab">
     <a class="nav-item nav-link {{ Request::is('*index') ? 'active' : '' }}" href="{{ route('user.uhome', $user->id) }}"
        style="position:absolute; left:0;"
     >社区轨迹</a>
+
+    <a class="nav-item nav-link {{ Request::is('*topic-published') ? 'active' : '' }}" href="{{ route('user.uhome.topic-published', $user->id) }}">发起的话题</a>
+    <a class="nav-item nav-link {{ Request::is('*topic-replies') ? 'active' : '' }}" href="{{ route('user.uhome.topic-replies', $user->id) }}">参与的话题</a>
+
+    <a class="nav-item nav-link {{ Request::is('*activity') ? 'active' : '' }}" href="{{ route('user.uhome.activity', $user->id) }}">发起或参与的活动</a>
+
+    @if ($user->columnist)
+        <a class="nav-item nav-link" href="{{ route('columnist.show', $user->columnist->domain) }}">专栏</a>
+    @endif
+</nav>
+
+<nav class="d-flex d-md-none nav nav-pills justify-content-center">
+    <a class="nav-item nav-link {{ Request::is('*index') ? 'active' : '' }}" href="{{ route('user.uhome', $user->id) }}">社区轨迹</a>
 
     <a class="nav-item nav-link {{ Request::is('*topic-published') ? 'active' : '' }}" href="{{ route('user.uhome.topic-published', $user->id) }}">发起的话题</a>
     <a class="nav-item nav-link {{ Request::is('*topic-replies') ? 'active' : '' }}" href="{{ route('user.uhome.topic-replies', $user->id) }}">参与的话题</a>
