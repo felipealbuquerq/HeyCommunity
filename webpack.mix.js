13f6999da26cb3ae5f175a7d21d8b7351c92eb08
Rod <supgeek.rod@gmail.com>
2019-04-23 17:51:03 +0800

    Cropper.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,6 +15,8 @@ mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .sass('resources/assets/sass-admin/admin.scss', 'public/css');
 
+mix.copy('node_modules', 'public/assets/node_modules');
+
 //
 // asset version
 if (mix.inProduction()) {
