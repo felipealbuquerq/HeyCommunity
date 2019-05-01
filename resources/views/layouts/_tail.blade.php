cb7daa9d234945598937ca219edf757756f40d3d
Rod <supgeek.rod@gmail.com>
2019-05-01 23:06:43 +0800

    modify site tail

--- resources/views/layouts/_tail.blade.php
+++ resources/views/layouts/_tail.blade.php
@@ -1,7 +1,7 @@
 <div class="card card-link-list mt-3">
     <div class="card-body">
         <!-- Site Map -->
-        <div class="d-none">
+        <div class="">
             <a class="ml-0" href="{{ route('news.index') }}">新闻</a>
             <a href="{{ route('daily.index') }}">今日</a>
             <a href="{{ route('post.index') }}">资讯</a>
@@ -10,16 +10,20 @@
             <a href="{{ route('activity.index') }}">活动</a>
         </div>
 
-        <div class="text-nowrap text-overflow">
-            &copy; 2018 {{ $system->site_title }}
-            <small>发现和创造美好</small>
-        </div>
-
-        <div>
+        <div class="mt-1">
             <a class="ml-0" href="{{ route('site.about') }}">关于</a>
             <a href="{{ route('site.help') }}">帮助</a>
             <a href="{{ route('site.terms') }}">用户协议</a>
             <a href="{{ route('site.privacy') }}">隐私政策</a>
+        </div>
+
+        <div class="text-nowrap text-overflow mt-2">
+            <div class="">
+                &copy; 2017-{{ date('Y') }} {{ $system->site_title }}
+                <small>发现和创造美好</small>
+            </div>
+            <div>赣ICP备17002208号-2</div>
+            <div>Powered By <a target="_blank" href="https://www.hey-community.com">HeyCommunityV4</a></div>
         </div>
     </div>
 </div>
