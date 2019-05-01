f717a883870f612f913b1e94d293d8afc6fc4d56
Rod <supgeek.rod@gmail.com>
2019-05-02 00:10:05 +0800

    modify site tail

--- resources/views/layouts/_tail.blade.php
+++ resources/views/layouts/_tail.blade.php
@@ -1,25 +1,27 @@
 <div class="card card-link-list mt-3">
     <div class="card-body">
         <!-- Site Map -->
-        <div class="d-none">
+        <div class="">
             <a class="ml-0" href="{{ route('news.index') }}">新闻</a>
-            <a href="{{ route('daily.index') }}">今日</a>
-            <a href="{{ route('post.index') }}">资讯</a>
+            <a href="{{ route('timeline.index') }}">动态</a>
             <a href="{{ route('columnist.index') }}">专栏</a>
             <a href="{{ route('topic.index') }}">话题</a>
             <a href="{{ route('activity.index') }}">活动</a>
         </div>
 
-        <div class="text-nowrap text-overflow">
-            &copy; 2018 {{ $system->site_title }}
-            <small>免费开源的社交软件</small>
-        </div>
-
-        <div>
+        <div class="mt-1">
             <a class="ml-0" href="{{ route('site.about') }}">关于</a>
             <a href="{{ route('site.help') }}">帮助</a>
             <a href="{{ route('site.terms') }}">用户协议</a>
             <a href="{{ route('site.privacy') }}">隐私政策</a>
+        </div>
+
+        <div class="text-nowrap text-overflow mt-2">
+            <div class="">
+                &copy; 2017-{{ date('Y') }} {{ $system->site_title }}
+                <small>免费开源的社交软件</small>
+            </div>
+            <div>Powered By <a target="_blank" href="https://www.hey-community.com">HeyCommunityV4</a></div>
         </div>
     </div>
 </div>
